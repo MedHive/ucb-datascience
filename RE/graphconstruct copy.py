c4d45dcfa3223b3ee3824b03d636f50aa3f2ffff
willthewuberkeley <williamzwu@berkeley.edu>
2025-11-05 17:32:27 -0800

    added more data for each node, pipelined all data

---
+++ RE/graphconstruct copy.py
@@ -0,0 +1,192 @@
+"""
+(9, 11): {'relation': 'point in time', 'head_span': Letter of March 2, 2006, 'tail_span': March 2, 2006}
+(28, 116): {'relation': 'has part', 'head_span': Capnostream, 'tail_span': Capnostream 20}
+(116, 28): {'relation': 'part of', 'head_span': Capnostream 20, 'tail_span': Capnostream}
+(116, 44): {'relation': 'followed by', 'head_span': Capnostream 20, 'tail_span': Capnostream 10}
+(44, 116): {'relation': 'follows', 'head_span': Capnostream 10, 'tail_span': Capnostream 20}
+(73, 81): {'relation': 'headquarters location', 'head_span': Oridion Capnography Inc., 'tail_span': Needham, MA}
+(104, 108): {'relation': 'has part', 'head_span': Capnostreamn, 'tail_span': Capnostreaml}
+(104, 111): {'relation': 'has part', 'head_span': Capnostreamn, 'tail_span': CapnostroaM}
+(108, 104): {'relation': 'part of', 'head_span': Capnostreaml, 'tail_span': Capnostreamn}
+(108, 111): {'relation': 'part of', 'head_span': Capnostreaml, 'tail_span': CapnostroaM}
+(111, 104): {'relation': 'part of', 'head_span': CapnostroaM, 'tail_span': Capnostreamn}
+(111, 108): {'relation': 'part of', 'head_span': CapnostroaM, 'tail_span': Capnostreaml}
+(162, 164): {'relation': 'inception', 'head_span': Oridion Medical 1987 Ltd., 'tail_span': 1987}
+(178, 180): {'relation': 'country', 'head_span': Jerusalem, 'tail_span': Israel}
+(227, 230): {'relation': 'instance of', 'head_span': B051971, 'tail_span': DESCRIPTIONS}
+(116, 124): {'relation': 'instance of', 'head_span': Capnostream 20, 'tail_span': Monitor}
+(283, 285): {'relation': 'point in time', 'head_span': Oridion Polaris 2004, 'tail_span': 2004}
+(433, 328): {'relation': 'instance of', 'head_span': Capnostream20, 'tail_span': Pulse Oximeter}
+(28, 446): {'relation': 'instance of', 'head_span': Capnostream, 'tail_span': capnograph}
+(513, 515): {'relation': 'subclass of', 'head_span': neonatal, 'tail_span': pediatric}
+(239, 123): {'relation': 'instance of', 'head_span': Capnostream 20bedside monitor, 'tail_span': Bedside Monitor}
+(628, 458): {'relation': 'subclass of', 'head_span': critical care, 'tail_span': health care}
+(642, 628): {'relation': 'subclass of', 'head_span': neonatal Intensive care, 'tail_span': critical care}
+(479, 481): {'relation': 'has part', 'head_span': breath, 'tail_span': respiration}
+(481, 479): {'relation': 'part of', 'head_span': respiration, 'tail_span': breath}
+(122, 695): {'relation': 'instance of', 'head_span': Parameter Bedside Monitor, 'tail_span': ahost device}
+(494, 499): {'relation': 'facet of', 'head_span': functional oxygen saturation, 'tail_span': hemoglobin}
+(628, 635): {'relation': 'studied by', 'head_span': critical care, 'tail_span': anesthesiology}
+(635, 628): {'relation': 'studies', 'head_span': anesthesiology, 'tail_span': critical care}
+(122, 123): {'relation': 'instance of', 'head_span': Parameter, 'tail_span': Bedside Monitor}
+(962, 123): {'relation': 'instance of', 'head_span': Capnostreamlo, 'tail_span': Bedside Monitor}
+(283, 292): {'relation': 'manufacturer', 'head_span': Oridion Polaris 2004, 'tail_span': CAS Medical Systems, Inc}
+(44, 419): {'relation': 'instance of', 'head_span': Capnostream 10, 'tail_span': carbon dioxide analyzer}
+(44, 323): {'relation': 'use', 'head_span': Capnostream 10, 'tail_span': Monitoring}
+(1164, 1169): {'relation': 'has part', 'head_span': Capnostream 10Bedside Monitor, 'tail_span': MinMediCO 2EtCO2module}
+(1169, 1164): {'relation': 'part of', 'head_span': MinMediCO 2EtCO2module, 'tail_span': Capnostream 10Bedside Monitor}
+(329, 1407): {'relation': 'inception', 'head_span': Oximeter, 'tail_span': April 6, 2006}
+(1578, 1580): {'relation': 'subclass of', 'head_span': misbranding, 'tail_span': adulteration}
+(1629, 1634): {'relation': 'has part', 'head_span': Code of Federal Regulations, 'tail_span': Title 21}
+(1634, 1629): {'relation': 'part of', 'head_span': Title 21, 'tail_span': Code of Federal Regulations}
+(206, 12): {'relation': 'number of episodes', 'head_span': Page, 'tail_span': 2}
+(1701, 1708): {'relation': 'main subject', 'head_span': Federal statutes and regulations, 'tail_span': Federal agencies}
+(415, 1734): {'relation': 'has part', 'head_span': 21 CFR, 'tail_span': 21 CFR Part 807}
+(415, 1742): {'relation': 'has part', 'head_span': 21 CFR, 'tail_span': 21 CFR Part 801}
+(415, 1765): {'relation': 'has part', 'head_span': 21 CFR, 'tail_span': 21 CFR Part 820}
+(415, 1793): {'relation': 'has part', 'head_span': 21 CFR, 'tail_span': 21 CFR 1000-1050}
+(1734, 415): {'relation': 'part of', 'head_span': 21 CFR Part 807, 'tail_span': 21 CFR}
+(1742, 415): {'relation': 'part of', 'head_span': 21 CFR Part 801, 'tail_span': 21 CFR}
+(1765, 415): {'relation': 'part of', 'head_span': 21 CFR Part 820, 'tail_span': 21 CFR}
+(1793, 415): {'relation': 'part of', 'head_span': 21 CFR 1000-1050, 'tail_span': 21 CFR}
+(19, 1432): {'relation': 'subclass of', 'head_span': 510(k), 'tail_span': notification}
+(1835, 392): {'relation': 'instance of', 'head_span': predicate device, 'tail_span': CLASSIFICATION}
+(1868, 1742): {'relation': 'has part', 'head_span': labeling regulation, 'tail_span': 21 CFR Part 801}
+(1742, 1868): {'relation': 'part of', 'head_span': 21 CFR Part 801, 'tail_span': labeling regulation}
+(1902, 1734): {'relation': 'part of', 'head_span': Misbranding by reference to premarket notification, 'tail_span': 21 CFR Part 807}
+(1734, 1902): {'relation': 'has part', 'head_span': 21 CFR Part 807, 'tail_span': Misbranding by reference to premarket notification}
+(1932, 8): {'relation': 'parent organization', 'head_span': Division of Small Manufacturers, International and Consumer Assistance, 'tail_span': FDA}
+(8, 1932): {'relation': 'subsidiary', 'head_span': FDA, 'tail_span': Division of Small Manufacturers, International and Consumer Assistance}
+(635, 1984): {'relation': 'part of', 'head_span': anesthesiology, 'tail_span': General Hospital}
+(116, 250): {'relation': 'use', 'head_span': Capnostream 20, 'tail_span': EtCO2}
+(962, 1077): {'relation': 'use', 'head_span': Capnostreamlo, 'tail_span': C02 indications}
+(2256, 2271): {'relation': 'part of', 'head_span': Prescription Use X AND/OR Over-The-Counter Use, 'tail_span': 21 CFR 801 Subpart D}
+(2271, 2256): {'relation': 'has part', 'head_span': 21 CFR 801 Subpart D, 'tail_span': Prescription Use X AND/OR Over-The-Counter Use}
+(400, 1344): {'relation': 'different from', 'head_span': I, 'tail_span': O}
+(1344, 400): {'relation': 'different from', 'head_span': O, 'tail_span': I}
+"""
+
+import re
+import csv
+
+text = """
+(9, 11): {'relation': 'point in time', 'head_span': Letter of March 2, 2006, 'tail_span': March 2, 2006}
+(28, 116): {'relation': 'has part', 'head_span': Capnostream, 'tail_span': Capnostream 20}
+(116, 28): {'relation': 'part of', 'head_span': Capnostream 20, 'tail_span': Capnostream}
+(116, 44): {'relation': 'followed by', 'head_span': Capnostream 20, 'tail_span': Capnostream 10}
+(44, 116): {'relation': 'follows', 'head_span': Capnostream 10, 'tail_span': Capnostream 20}
+(73, 81): {'relation': 'headquarters location', 'head_span': Oridion Capnography Inc., 'tail_span': Needham, MA}
+(104, 108): {'relation': 'has part', 'head_span': Capnostreamn, 'tail_span': Capnostreaml}
+(104, 111): {'relation': 'has part', 'head_span': Capnostreamn, 'tail_span': CapnostroaM}
+(108, 104): {'relation': 'part of', 'head_span': Capnostreaml, 'tail_span': Capnostreamn}
+(108, 111): {'relation': 'part of', 'head_span': Capnostreaml, 'tail_span': CapnostroaM}
+(111, 104): {'relation': 'part of', 'head_span': CapnostroaM, 'tail_span': Capnostreamn}
+(111, 108): {'relation': 'part of', 'head_span': CapnostroaM, 'tail_span': Capnostreaml}
+(162, 164): {'relation': 'inception', 'head_span': Oridion Medical 1987 Ltd., 'tail_span': 1987}
+(178, 180): {'relation': 'country', 'head_span': Jerusalem, 'tail_span': Israel}
+(227, 230): {'relation': 'instance of', 'head_span': B051971, 'tail_span': DESCRIPTIONS}
+(116, 124): {'relation': 'instance of', 'head_span': Capnostream 20, 'tail_span': Monitor}
+(283, 285): {'relation': 'point in time', 'head_span': Oridion Polaris 2004, 'tail_span': 2004}
+(433, 328): {'relation': 'instance of', 'head_span': Capnostream20, 'tail_span': Pulse Oximeter}
+(28, 446): {'relation': 'instance of', 'head_span': Capnostream, 'tail_span': capnograph}
+(513, 515): {'relation': 'subclass of', 'head_span': neonatal, 'tail_span': pediatric}
+(239, 123): {'relation': 'instance of', 'head_span': Capnostream 20bedside monitor, 'tail_span': Bedside Monitor}
+(628, 458): {'relation': 'subclass of', 'head_span': critical care, 'tail_span': health care}
+(642, 628): {'relation': 'subclass of', 'head_span': neonatal Intensive care, 'tail_span': critical care}
+(479, 481): {'relation': 'has part', 'head_span': breath, 'tail_span': respiration}
+(481, 479): {'relation': 'part of', 'head_span': respiration, 'tail_span': breath}
+(122, 695): {'relation': 'instance of', 'head_span': Parameter Bedside Monitor, 'tail_span': ahost device}
+(494, 499): {'relation': 'facet of', 'head_span': functional oxygen saturation, 'tail_span': hemoglobin}
+(628, 635): {'relation': 'studied by', 'head_span': critical care, 'tail_span': anesthesiology}
+(635, 628): {'relation': 'studies', 'head_span': anesthesiology, 'tail_span': critical care}
+(122, 123): {'relation': 'instance of', 'head_span': Parameter, 'tail_span': Bedside Monitor}
+(962, 123): {'relation': 'instance of', 'head_span': Capnostreamlo, 'tail_span': Bedside Monitor}
+(283, 292): {'relation': 'manufacturer', 'head_span': Oridion Polaris 2004, 'tail_span': CAS Medical Systems, Inc}
+(44, 419): {'relation': 'instance of', 'head_span': Capnostream 10, 'tail_span': carbon dioxide analyzer}
+(44, 323): {'relation': 'use', 'head_span': Capnostream 10, 'tail_span': Monitoring}
+(1164, 1169): {'relation': 'has part', 'head_span': Capnostream 10Bedside Monitor, 'tail_span': MinMediCO 2EtCO2module}
+(1169, 1164): {'relation': 'part of', 'head_span': MinMediCO 2EtCO2module, 'tail_span': Capnostream 10Bedside Monitor}
+(329, 1407): {'relation': 'inception', 'head_span': Oximeter, 'tail_span': April 6, 2006}
+(1578, 1580): {'relation': 'subclass of', 'head_span': misbranding, 'tail_span': adulteration}
+(1629, 1634): {'relation': 'has part', 'head_span': Code of Federal Regulations, 'tail_span': Title 21}
+(1634, 1629): {'relation': 'part of', 'head_span': Title 21, 'tail_span': Code of Federal Regulations}
+(206, 12): {'relation': 'number of episodes', 'head_span': Page, 'tail_span': 2}
+(1701, 1708): {'relation': 'main subject', 'head_span': Federal statutes and regulations, 'tail_span': Federal agencies}
+(415, 1734): {'relation': 'has part', 'head_span': 21 CFR, 'tail_span': 21 CFR Part 807}
+(415, 1742): {'relation': 'has part', 'head_span': 21 CFR, 'tail_span': 21 CFR Part 801}
+(415, 1765): {'relation': 'has part', 'head_span': 21 CFR, 'tail_span': 21 CFR Part 820}
+(415, 1793): {'relation': 'has part', 'head_span': 21 CFR, 'tail_span': 21 CFR 1000-1050}
+(1734, 415): {'relation': 'part of', 'head_span': 21 CFR Part 807, 'tail_span': 21 CFR}
+(1742, 415): {'relation': 'part of', 'head_span': 21 CFR Part 801, 'tail_span': 21 CFR}
+(1765, 415): {'relation': 'part of', 'head_span': 21 CFR Part 820, 'tail_span': 21 CFR}
+(1793, 415): {'relation': 'part of', 'head_span': 21 CFR 1000-1050, 'tail_span': 21 CFR}
+(19, 1432): {'relation': 'subclass of', 'head_span': 510(k), 'tail_span': notification}
+(1835, 392): {'relation': 'instance of', 'head_span': predicate device, 'tail_span': CLASSIFICATION}
+(1868, 1742): {'relation': 'has part', 'head_span': labeling regulation, 'tail_span': 21 CFR Part 801}
+(1742, 1868): {'relation': 'part of', 'head_span': 21 CFR Part 801, 'tail_span': labeling regulation}
+(1902, 1734): {'relation': 'part of', 'head_span': Misbranding by reference to premarket notification, 'tail_span': 21 CFR Part 807}
+(1734, 1902): {'relation': 'has part', 'head_span': 21 CFR Part 807, 'tail_span': Misbranding by reference to premarket notification}
+(1932, 8): {'relation': 'parent organization', 'head_span': Division of Small Manufacturers, International and Consumer Assistance, 'tail_span': FDA}
+(8, 1932): {'relation': 'subsidiary', 'head_span': FDA, 'tail_span': Division of Small Manufacturers, International and Consumer Assistance}
+(635, 1984): {'relation': 'part of', 'head_span': anesthesiology, 'tail_span': General Hospital}
+(116, 250): {'relation': 'use', 'head_span': Capnostream 20, 'tail_span': EtCO2}
+(962, 1077): {'relation': 'use', 'head_span': Capnostreamlo, 'tail_span': C02 indications}
+(2256, 2271): {'relation': 'part of', 'head_span': Prescription Use X AND/OR Over-The-Counter Use, 'tail_span': 21 CFR 801 Subpart D}
+(2271, 2256): {'relation': 'has part', 'head_span': 21 CFR 801 Subpart D, 'tail_span': Prescription Use X AND/OR Over-The-Counter Use}
+(400, 1344): {'relation': 'different from', 'head_span': I, 'tail_span': O}
+(1344, 400): {'relation': 'different from', 'head_span': O, 'tail_span': I}
+"""
+
+pattern = re.compile(
+    r"'relation'\s*:\s*'(?P<relation>[^']+)"  # relation quoted
+    r"'\s*,\s*'head_span'\s*:\s*(?P<head>.*?)(?=,\s*'tail_span'\s*:)"  # head up to the tail marker
+    r"\s*,\s*'tail_span'\s*:\s*(?P<tail>.*?)(?=})",
+    re.DOTALL,
+)
+
+edges = []
+
+for match in pattern.finditer(text):
+    relation = match.group('relation')
+    head = match.group('head')
+    tail = match.group('tail')
+
+    # Clean up surrounding whitespace and possible surrounding quotes
+    def clean(s):
+        s = s.strip()
+        if (s.startswith("'") and s.endswith("'")) or (s.startswith('"') and s.endswith('"')):
+            s = s[1:-1]
+        return s.strip()
+
+    edges.append({
+        'head_span': clean(head),
+        'tail_span': clean(tail),
+        'relation': clean(relation),
+    })
+
+# Now all values are strings
+for e in edges:
+    e['head_span'] = str(e['head_span'])
+    e['tail_span'] = str(e['tail_span'])
+
+nodes = set()
+for e in edges:
+    nodes.add(e['head_span'])
+    nodes.add(e['tail_span'])
+
+nodes = list(nodes)
+
+with open('nodes.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['id', 'name'])
+    for i, node in enumerate(nodes):
+        writer.writerow([i, node])
+
+# Create a mapping from node name to id
+node_id_map = {name: i for i, name in enumerate(nodes)}
+
+with open('edges.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['source', 'target', 'relation'])
+    for e in edges:
+        writer.writerow([node_id_map[e['head_span']], node_id_map[e['tail_span']], e['relation']])
